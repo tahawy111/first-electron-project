52bf938098036cb7f66bec2bc9da2a0a4430dfd8
tahawy111 <amertahawy111@gmail.com>
2024-11-26 07:01:28 +0200

    first commit

---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,19 @@
+import osUtils from "os-utils";
+
+const POLLING_INTERVAL = 500;
+
+export function pollResources() {
+  setInterval(async () => {
+    const cpuUsage = await getCpuUsage();
+    const ramUsage = getRamUsage();
+    console.log({ cpuUsage, ramUsage });
+  }, POLLING_INTERVAL);
+}
+
+function getCpuUsage() {
+  return new Promise((resolve) => osUtils.cpuUsage(resolve));
+}
+
+function getRamUsage() {
+  return 1 - osUtils.freememPercentage();
+}
